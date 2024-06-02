e5a2bcb64bccf77f2c5c64728da7bc2ac8d8bd41
yuito723 <main@yuito723.net>
2024-06-02 20:13:05 +0900

    20240602

--- genkey.py
+++ genkey.py
@@ -1,5 +1,4 @@
 import subprocess as sub
-import time
 
 # capture_output = True
 
@@ -10,41 +9,8 @@ client_number = 4
 sub.run("wg genkey > server.key", shell = True)
 sub.run("type server.key | wg pubkey > server.pub", shell = True)
 
-# output "client*.key", "client*.pub"
+# output "client*.key", "client*.pub", "client*-preshared.key"
 for i in range(client_number):
     sub.run(f"wg genkey > client{i + 2}.key", shell = True)
     sub.run(f"type client{i + 2}.key | wg pubkey > client{i + 2}.pub", shell = True)
-
-# output "client*-preshared.key"
-
-
-
-
-# server_key = sub.check_output("wg genkey").decode("utf8").strip()
-# server_pub = sub.check_output(["wg pubkey", "8NS3x1uIeFTo2JDCTln6WNLsAn8tCKgFi61S4HUapmo="]).decode("utf8").strip()
-
-# server_key = sub.run("wg", shell = True).decode("utf8").strip()
-
-# server_key = sub.run("wg genkey", shell = True)
-
-# server_key = sub.run("type sv.key | wg pubkey ", shell = True, capture_output = True, text = True)
-
-# server_key = sub.run(["wg", "pubkey", "8NS3x1uIeFTo2JDCTln6WNLsAn8tCKgFi61S4HUapmo="], capture_output = True, text = True)
-
-
-# print("#########################################################")
-# print(f"stdout : {server_key.stdout.strip()}")
-# print(f"stderr : {server_key.stderr.strip()}")
-# print("#########################################################")
-# time.sleep(3)
-
-
-# a = sub.check_output("wg genkey | tee server.key")
-
-# b = sub.check_output(f"wg pubkey {a}")
-
-# print(server_key)
-
-# print(server_pub)
-
-# print(a)
+    sub.run(f"wg genkey > client{i + 2}-preshared.key", shell = True)
